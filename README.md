e633fe2990a7daa90c58791fab8208988a05212a
RaphaelIsaiah <rafvie12@gmail.com>
2024-06-10 10:54:30 +0100

    Added script file. Successfully implemented the navPane functionality

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 A sample HTML documentation page.
+
+// <uniquifier>: Use a unique and descriptive class name
+// <weight>: Use a value from 100 to 900
+
+.montserrat-<uniquifier> {
+font-family: "Montserrat", sans-serif;
+font-optical-sizing: auto;
+font-weight: <weight>;
+font-style: normal;
+}
+
+// <uniquifier>: Use a unique and descriptive class name
+// <weight>: Use a value from 200 to 700
+
+.oswald-<uniquifier> {
+font-family: "Oswald", sans-serif;
+font-optical-sizing: auto;
+font-weight: <weight>;
+font-style: normal;
+}
+
+// <uniquifier>: Use a unique and descriptive class name
+// <weight>: Use a value from 100 to 700
+
+.roboto-mono-<uniquifier> {
+font-family: "Roboto Mono", monospace;
+font-optical-sizing: auto;
+font-weight: <weight>;
+font-style: normal;
+}
--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-let navBar = document.getElementById("mobileNav");
+let navPane = document.getElementById("nav-pane");
+let mobHeader = document.getElementById("mob-header")
 
 function openNav() {
-  navBar.style.width = "100%";
+  navPane.style.display = "block";
+  navPane.style.width = "100%";
 }
 
 function closeNav() {
-  navBar.style.width = "0%";
+  navPane.style.width = "0%";
+  navPane.style.display = "none";
 }
+
+const handleMediaQuery = (x) => {
+  if (x.matches) {
+    // If the media query matches (viewport width <= 760px)
+    // At this point the hamburger icon will display according to the css styling rule set.
+    // And the main navigation menu for viewports > 760px will not be displayed.
+   mobHeader.style.display = "flex"
+   navPane.style.display = "none"
+  } else {
+    mobHeader.style.display = "none"
+    navPane.style.display = "block"
+    // Otherwise (viewport width > 760px)
+  }
+};
+
+// Create a MediaQueryList object for the specified media query
+var x = window.matchMedia("(max-width: 768px)");
+
+// Call the listener function initially
+handleMediaQuery(x);
+
+// Attach the listener function to changes in the media query state
+x.addEventListener("change", handleMediaQuery);
